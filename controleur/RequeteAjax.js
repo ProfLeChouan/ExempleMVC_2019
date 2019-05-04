7bd2fe1541bba22c6ac6711d91dd628e3ec83e99
prof-dev1 <proflechouan.dev1@gmail.com>
2019-05-04 10:18:19 -0400

    Encapsuler la partie definition de la requete dans le constructeur et la partie execution (open send) dans une methode 2 constructeurs de requete ajax Requete ajax a partir d un url et d un callback
    Requete ajax a patrir d un url et d un modele ahuntsic

--- controleur/RequeteAjax.js
+++ controleur/RequeteAjax.js
@@ -2,18 +2,28 @@
 
 class RequeteAjax {
 
-  constructor(url) {
-    this.url = url;
-  }
-
-  getJSON(callback) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+  constructor(url, callback) {
+	this.url = url;
+    this.xhttp = new XMLHttpRequest();
+    this.xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         callback(this.responseText);
       }
     };
-    xhttp.open("GET", this.url, true);
-    xhttp.send();
+
   }
+  
+  openAndSend() {
+    this.xhttp.open("GET", this.url, true);
+	this.xhttp.send();
+  }
+
+}
+
+class RequeteAjaxAhuntsicModele extends RequeteAjax {
+
+  constructor(url, modele) {
+	super(url, data => modele.applyTemplate_toAll(data));
+  }
+ 
 }
